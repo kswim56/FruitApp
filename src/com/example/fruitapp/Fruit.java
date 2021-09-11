bd0af27e7fd9028607350c8a42aabbe61cdeb4b5
kswim56 <21800108@handong.edu>
2021-09-11 22:48:39 +0900

    This is my first commit for FruitApp

--- src/com/example/fruitapp/Fruit.java
+++ src/com/example/fruitapp/Fruit.java
@@ -1,15 +1,17 @@
 package com.example.fruitapp;
+import java.util.Comparator;
 
-public class Fruit implements Comparable<Fruit> {
+public class Fruit{
 	int no;
 	String name;
+	int prefer;
 	
-	public Fruit() {}
-	public Fruit(int no, String name) {
+	public Fruit(int no, String name, int prefer) {
 		super(); 
 		//부모 클래스의 멤버를 초기화하기 위해 자바 컴파일러가 부모 클래스의 생성자를 명시적으로 호출하지 않는 모든 자식 클래스의 생성자 첫 줄에 자동으로 입력해줌.
 		this.no = no;
 		this.name = name;
+		this.prefer = prefer;
 		//클래스의 속성과 생성자/메소드의 매개변수(input parameter)의 이름이 같은 경우
 		//클래스 속성을 사용할 때 this 키워드를 붙여준다.
 		
@@ -30,16 +32,26 @@ public class Fruit implements Comparable<Fruit> {
 		this.name = name;
 	}
 	
+	public int getPrefer() {
+		return prefer;
+	}
+	public void setPrefer(int prefer) {
+		this.prefer = prefer;
+	}
 	@Override
 	public String toString() {
-		return "Fruit [no=" + no + ", name=" + name + "]";
-	}
-	
-	@Override
-	public int compareTo(Fruit o) {
-		return name.compareTo(o.name);
+		return "Fruit [no=" + no + ", name=" + name + ", prefer=" + prefer + "]";
 	}
 	
 	
 
 }
+class FruitComparator implements Comparator<Fruit>{
+	@Override
+	public int compare(Fruit o1, Fruit o2) {
+		return o1.name.compareTo(o2.name);
+	}
+	
+}
+
+
--- src/com/example/fruitapp/Main.java
+++ src/com/example/fruitapp/Main.java
@@ -9,23 +9,21 @@ public class Main {
 		//create arraylist of user-defined class objects
 		List<Fruit> list = new ArrayList<Fruit>();
 		System.out.println("Student List (orderd by name)");
-		Fruit s = new Fruit();
-		list.add(s); //adding Student class object
-		list.add(new Fruit(1, "rambutan"));
-		list.add(new Fruit(2, "apple"));
-		list.add(new Fruit(3, "banana"));
-		list.add(new Fruit(4, "strewberry"));
-		list.add(new Fruit(5, "peach"));
-		list.add(new Fruit(6, "watermelon"));
-		list.add(new Fruit(7, "durian"));
-		list.add(new Fruit(8, "orange"));
-		list.add(new Fruit(9, "cherry"));
-		list.add(new Fruit(10, "grape"));
-		list.add(new Fruit(11, "pineapple"));
-		list.add(new Fruit(12, "coconut"));
+		list.add(new Fruit(1, "rambutan", 50));
+		list.add(new Fruit(2, "apple", 80));
+		list.add(new Fruit(3, "banana", 70));
+		list.add(new Fruit(4, "strewberry", 90));
+		list.add(new Fruit(5, "peach", 100));
+		list.add(new Fruit(6, "watermelon", 75));
+		list.add(new Fruit(7, "durian",20));
+		list.add(new Fruit(8, "orange",60));
+		list.add(new Fruit(9, "cherry",45));
+		list.add(new Fruit(10, "grape", 65));
+		list.add(new Fruit(11, "pineapple", 35));
+		list.add(new Fruit(12, "coconut", 20));
 		
 		
-		Collections.sort(list);
+		Collections.sort(list, new FruitComparator());
 		for(Fruit a : list) {
 			System.out.println(a.toString());
 		}
